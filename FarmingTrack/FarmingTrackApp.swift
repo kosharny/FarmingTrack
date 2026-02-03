c0c91bd41657cd8b462b9e1ac539f408fe23ec1d
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-03 13:35:25 +0800

    Initial Commit

---
+++ FarmingTrack/FarmingTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmingTrackApp.swift
+//  FarmingTrack
+//
+//  Created by Maksim Kosharny on 03.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FarmingTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
